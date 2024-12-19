b11b86f89cc07d224e4df1a22d613ebe9bd72c84
ElcanAydemirov <gkelcan@gmail.com>
2024-12-19 11:46:14 +0400

    React-StateProps uploaded

--- React-StateProps/src/components/Form/index.jsx
+++ React-StateProps/src/components/Form/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './index.css'
 import { nanoid } from 'nanoid'
+import Swal from 'sweetalert2';
+
 
 const Form = ({cName,btnName,setInput,setData,data}) => {
   
@@ -8,12 +10,29 @@ const Form = ({cName,btnName,setInput,setData,data}) => {
     <form action="" className='form' onSubmit={
       (e) => {
         e.preventDefault()
+        const inputValue = e.target.firstChild.value.trim();
+        if (inputValue == "") {
+          Swal.fire({
+            icon: "error",
+            title: "Inputlari doldurun",
+            text: "Inputlari doldurun",
+            footer: '<a href="#">Why do I have this issue?</a>'
+          });
+          return
+        }
         const obj = {
           id:nanoid(),
-          value:e.target.firstChild.value
+          value: inputValue
         }
         console.log(obj);      
        setData([...data,obj]);
+
+       Swal.fire({
+        title: "Added Succesfully",
+        icon: "success",
+        draggable: true
+      });
+       e.target.reset()
       }
     }>
       <input type="text" placeholder='task'/>
--- React-StateProps/src/components/List/index.jsx
+++ React-StateProps/src/components/List/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./index.css";
-import { FaTrash } from "react-icons/fa";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const List = ({ data, setData }) => {
   return (
@@ -10,11 +11,63 @@ const List = ({ data, setData }) => {
           <ul key={task.id} data-id={task.id}>
             <li>
               <span>{task.value}</span>
-              <span>
+              <span style={{ display: "flex", gap: "0.5rem" }}>
                 <FaTrash
                   style={{ color: "red", cursor: "pointer" }}
                   onClick={(e) => {
-                  setData([...data.filter((task) => task.id !== e.target.closest("ul").getAttribute("data-id"))])  
+                    Swal.fire({
+                      title: "Are you sure?",
+                      text: "Are you sure to delete?",
+                      icon: "warning",
+                      showCancelButton: true,
+                      confirmButtonColor: "#3085d6",
+                      cancelButtonColor: "#d33",
+                      confirmButtonText: "Yes"
+                    }).then((result) => {
+                      if (result.isConfirmed) {
+                        setData(data.filter((t) => t.id !== task.id)); // Task silinir
+                        Swal.fire({
+                          title: "Deleted!",
+                          text: "Your task has been deleted.",
+                          icon: "success"
+                        });
+                      }
+                    });
+                  }}
+                />
+                <FaEdit
+                  style={{ color: "blue", cursor: "pointer" }}
+                  onClick={() => {
+                    Swal.fire({
+                      title: "Edit Task",
+                      input: "text",
+                      inputValue: task.value, // Mövcud dəyər
+                      inputAttributes: {
+                        autocapitalize: "off"
+                      },
+                      showCancelButton: true,
+                      confirmButtonText: "Save",
+                      preConfirm: (editedValue) => {
+                        if (!editedValue.trim()) {
+                          Swal.showValidationMessage("Task cannot be empty!");
+                          return false;
+                        }
+                        return editedValue;
+                      }
+                    }).then((result) => {
+                      if (result.isConfirmed) {
+                        setData(
+                          data.map((t) =>
+                            t.id === task.id ? { ...t, value: result.value } : t
+                          )
+                        ); // Task yenilənir
+                        Swal.fire({
+                          title: "Updated!",
+                          text: "Your task has been updated.",
+                          icon: "success"
+                        });
+                      }
+                    });
                   }}
                 />
               </span>
